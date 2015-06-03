499350ccce6d64593627f09cb104a359655fa223
Phuc Cuong <pcuongnn@gmail.com>
2015-06-03 13:10:35 +0700

    bootstrap-1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :get_all_categories_authors
+
+  def get_all_categories_authors
+  	@categories = Category.all
+  	@authors = Author.all
+  end
 end
