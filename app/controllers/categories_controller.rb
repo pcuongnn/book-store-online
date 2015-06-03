4cecf1f5d50a1e6cefb431d2a17b250fde3f0e28
Phuc Cuong <pcuongnn@gmail.com>
2015-06-03 13:44:32 +0700

    using-partials

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @category_book_list = @category.books
   end
 
   # GET /categories/new
